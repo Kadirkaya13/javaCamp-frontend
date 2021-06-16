7e3ce8344397e5af947ccb911997dc8e363ac0cd
kadir kaya <kadirkaya.13@outlook.com.tr>
2021-06-17 00:58:27 +0300

    first commit

---
+++ src/layout/Navi.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import CartSummary from "./CartSummary";
+import {  Container, Menu } from "semantic-ui-react";
+import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
+import { useHistory } from "react-router";
+import { useSelector } from 'react-redux';
+
+export default function Navi() {
+  const {cartItems} = useSelector(state => state.cart)
+  const [isAuthenticated, setisAuthenticated] = useState(true)
+  const history = useHistory()
+
+  function handleSignOut() {
+    setisAuthenticated(false)
+    history.push("/")
+  }
+  function handleSignIn() {
+    setisAuthenticated(true)
+  }
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
+
+          <Menu.Menu position="right">
+            {cartItems.length>0&&<CartSummary/>}
+            {isAuthenticated?<SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>}
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
